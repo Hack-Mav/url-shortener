690150cb820caa0d8caf515936f775a7bcf8b500
parthiv <parthiv05022000@gmail.com>
2024-12-07 00:38:18 +0530

    modified files to ignore in .gitignore and .gcloudignore

--- config/datastore.go
+++ config/datastore.go
@@ -9,7 +9,7 @@ import (
 
 func ConnectDatastore() *datastore.Client {
 	ctx := context.Background()
-	client, err := datastore.NewClient(ctx, "genericissuer")
+	client, err := datastore.NewClient(ctx, "argon-magnet-442917-k1")
 	if err != nil {
 		log.Fatalf("Failed to connect to Google Cloud Datastore: %v", err)
 	}
